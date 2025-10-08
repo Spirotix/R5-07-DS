26ba3164acab851e776a2852fb73eaab64783e12
Spirotix <spirotixgaming@gmail.com>
2025-10-08 16:07:08 +0200

    Correction des 2 issues

---
+++ Q5.java
@@ -0,0 +1,20 @@
+public class Q5 
+{
+	public static void main(String[] args)
+	{
+		if (args.length != 1) 
+		{
+			System.err.println("Usage: java Q4 [N]");
+			System.exit(1);
+		}
+		int N = Integer.parseInt(args[0]);
+		for (int i = N; i >= 1; i--) 
+		{
+			for (int j = i; j <= i; j++) 
+			{
+				System.out.print("X" + " ");
+			}
+			System.out.println();
+		}
+	}
+}
